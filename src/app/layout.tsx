b3071af9447864599eec32d0d4cdf49d6a4de745
Leonardo T. S. <66446331+devLeonardoTS@users.noreply.github.com>
2025-02-19 20:00:53 -0300

    add: message list example

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang={AppConfigs.locale}>
       <AppProviders>
         <body className={`antialiased`}>{children}</body>
       </AppProviders>
